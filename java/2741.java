d7344c7e296990143e8c3822f3966e8aad1e5d4b
ahnjisu <71385038+ahnjisu@users.noreply.github.com>
2020-09-24 21:40:38 +0900

    N까지 출력

---
+++ java/2741.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		int sum = 0;
+
+		for (int i = 1; i <= N; i++) {
+
+			System.out.println(i);
+		}
+	}
+}
