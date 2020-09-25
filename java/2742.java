1b75072ca51a4a246ddeadc8cc30dfd16a163c0f
ahnjisu <71385038+ahnjisu@users.noreply.github.com>
2020-09-25 19:05:38 +0900

    Create 2742.java

---
+++ java/2742.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+
+		for (int i = N; i >= 1; i--) {
+
+			System.out.println(i);
+
+		}
+	}
+}
