40534491e7578c050b9ec9789fa36372b9c9efb0
ahnjisu <71385038+ahnjisu@users.noreply.github.com>
2020-09-29 23:31:33 +0900

    별찍기 오른쪽정렬

---
+++ java/2439.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double rate = 1100;
+		Scanner scanner = new Scanner(System.in);
+
+		int N = scanner.nextInt();
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (j + i <= N - 1)
+					System.out.print(" ");
+				else if (j + i > N - 1)
+					System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
