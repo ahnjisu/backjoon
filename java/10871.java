73da4c79a7b325a8503142ec87bde33b2994175c
ahnjisu <71385038+ahnjisu@users.noreply.github.com>
2020-09-30 23:04:58 +0900

    즐추

---
+++ java/10871.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+
+		int N = scanner.nextInt();
+		int X = scanner.nextInt();
+		int[] A = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			A[i] = scanner.nextInt();
+		}
+		for (int i = 0; i < N; i++) {
+			if (A[i] < X)
+				System.out.print(A[i] + " ");
+		}
+
+	}
+}
