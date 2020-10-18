f3e2d2942d0b94cac8fd0a915937085a661feb85
ahnjisu <71385038+ahnjisu@users.noreply.github.com>
2020-10-18 23:53:45 +0900

    아스키코드

---
+++ java/11654.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		String A = new String(); // 문자열 선언
+		A = scanner.next();
+
+		System.out.println((int) A.charAt(0)); // 문자를 char형 숫자로 변환
+
+	}
+}
